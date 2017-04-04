c523d7e7d1ac66e970847c4993e4a46e5cd403d0
Rick <rick@sammons.net>
2017-04-04 16:28:56 -0400

    wait for it to complete

--- BasicService/BasicService/Service1.cs
+++ BasicService/BasicService/Service1.cs
@@ -14,6 +14,7 @@ namespace BasicService
     public partial class Service1 : ServiceBase
     {
         int x = 0;
+        private bool isrunning = true;
         string _path = @"C:\junk\abc123.txt";
         TimeSpan ts = new TimeSpan(0, 5, 0);
         public Service1()
@@ -22,7 +23,7 @@ namespace BasicService
         }
         protected override void OnStart(string[] args)
         {
-            string _onStart = ("Service has begun " + DateTime.Now);
+            string _onStart = (string.Format("Service has begun {0}", DateTime.Now));
             if (File.Exists(_path))
             {
                 File.Delete(_path);
@@ -37,19 +38,27 @@ namespace BasicService
         }
         protected override void OnStop()
         {
+            x = 1;
+
+            while (isrunning)
+            {
+
+            }
+
             string _onStop = ("\r\nService has ended " + DateTime.Now);
             File.AppendAllText(_path, _onStop);
             Stop();
         }
         public void Execute()
         {
-            do
+            while (x == 0)
             {
                 string _onInterval = ("\r\nCheck complete " + DateTime.Now);
                 File.AppendAllText(_path, _onInterval);
                 Task.Delay(ts).Wait();
-            }
-            while (x == 0);
+            };
+
+            isrunning = false;
         }
         public void Start(string[] args)
         {
