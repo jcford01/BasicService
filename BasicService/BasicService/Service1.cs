efcb417091848e77bf41c1f78fd85bf5c1fdf0e2
Jesse <drofxtx@gmail.com>
2017-04-06 12:34:41 -0400

    implemented directory creation, fixed a few other things, switched build to console application

--- BasicService/BasicService/Service1.cs
+++ BasicService/BasicService/Service1.cs
@@ -15,38 +15,43 @@ namespace BasicService
     public partial class Service1 : ServiceBase
     {
         private bool _isRunning = true;
-        string _path = Path.Combine(Directory.GetCurrentDirectory() + @"Log\abc123.txt");
+        string _filePath = Path.Combine(Directory.GetCurrentDirectory(), "Log", "abc123.txt");
+        string _directoryPath = Path.Combine(Directory.GetCurrentDirectory(), "Log");
+        System.Timers.Timer _aTimer = new System.Timers.Timer(15000);
         public Service1()
         {
             InitializeComponent();
-            System.Timers.Timer _aTimer = new System.Timers.Timer(15000);
-            _aTimer.AutoReset = true;
-            _aTimer.Elapsed += Execute;
-            _aTimer.Start();
+            if (!Directory.Exists(_directoryPath))
+            {
+                Directory.CreateDirectory(_directoryPath);
+            }
         }
         protected override void OnStart(string[] args)
         {
             string _onStart = (string.Format("Service has begun {0}", DateTime.Now));
-            if (File.Exists(_path))
+            if (File.Exists(_filePath))
             {
-                File.Delete(_path);
+                File.Delete(_filePath);
             }
-            File.WriteAllText(_path, _onStart);
-            
+            File.WriteAllText(_filePath, _onStart);
+            _aTimer.AutoReset = true;
+            _aTimer.Elapsed += Execute;
+            _aTimer.Start();
         }
         protected override void OnStop()
         {
+            _aTimer.Stop();
             while (_isRunning)
             {
             }
             string _onStop = ("\r\nService has ended " + DateTime.Now);
-            File.AppendAllText(_path, _onStop);
+            File.AppendAllText(_filePath, _onStop);
         }
         public void Execute(object sender, ElapsedEventArgs e)
         {
             _isRunning = true;
             string _onInterval = ("\r\nCheck complete " + DateTime.Now);
-            File.AppendAllText(_path, _onInterval);
+            File.AppendAllText(_filePath, _onInterval);
             _isRunning = false;
         }
         public void Start(string[] args)
